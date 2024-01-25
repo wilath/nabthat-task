68aa2fad8d2040ffe3d18c5384686df548600ea8
Daniel <daniel.redzynski7@gmail.com>
2024-01-25 21:11:13 +0100

    change

--- n-task/src/app/shared/alphabet-sort.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AlphabetSortPipe } from './alphabet-sort.pipe';
-
-describe('AlphabetSortPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AlphabetSortPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
